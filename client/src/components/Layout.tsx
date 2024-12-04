064644b019c6f3a526b3db349c71f74c8b726e39
Vlad-2109 <vladswim21@gmail.com>
2024-12-04 23:00:49 +0200

    add navbar component

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import { Navbar } from './Navbar';
 
 export const Layout: React.FC = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
       <Outlet />
     </div>
   );
