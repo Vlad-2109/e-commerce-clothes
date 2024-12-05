324a6963cb1b59537e91206d3db06fd33e000f95
Vlad-2109 <vladswim21@gmail.com>
2024-12-05 22:22:41 +0200

    add policy component

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import { BestSeller } from '../../components/BestSeller';
 import { Hero } from '../../components/Hero';
 import { LatestCollection } from '../../components/LatestCollection';
+import { OurPolicy } from '../../components/OurPolicy';
 
 export const Home: React.FC = () => {
   return (
@@ -8,6 +9,7 @@ export const Home: React.FC = () => {
       <Hero />
       <LatestCollection />
       <BestSeller />
+      <OurPolicy />
     </div>
   );
 };
