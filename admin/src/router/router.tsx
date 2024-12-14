cf43b42438f3619e802023e0b68d0c7b78225bce
Vlad-2109 <vladswim21@gmail.com>
2024-12-14 19:55:33 +0200

    add layout component

--- admin/src/router/router.tsx
+++ admin/src/router/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Home } from '../pages/home/Home';
+import { Layout } from '../components/Layout';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      // { path: 'collection', element: <Collection /> },
+    ],
   },
 ]);
